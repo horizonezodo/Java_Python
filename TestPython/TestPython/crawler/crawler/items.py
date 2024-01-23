2b95ee21a54b0233c316dfc0052653e5b69d43f5
horizonezodo <thiensutoiloi3@gmail.com>
2024-01-23 10:05:59 +0700

    update python script

--- TestPython/TestPython/crawler/crawler/items.py
+++
@@ -1,8 +0,0 @@
-import scrapy
-
-class CrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    
-    title = scrapy.Field()
-    description = scrapy.Field()
-    sdt = scrapy.Field()
--- TestPython/TestPython/crawler/crawler/spiders/spidersConfig.py
+++
@@ -1,7 +0,0 @@
-spider_name = None
-domain = None
-start_url = None
-question_xpath = None
-title_xpath = None
-description_xpath = None
-sdt_xpath = None
